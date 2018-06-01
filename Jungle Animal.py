fe4fbc11073e8473ea9ef3cf8c38a273ab6a3b7b
Gaurav Padawe <grvpdw92@gmail.com>
2018-06-01 19:52:01 +0530

    Initial commit

---
+++ Jungle Animal.py
@@ -0,0 +1,14 @@
+﻿def jungle_animal(animal, my_speed):
+	while animal == "zebra":
+	    return "Try to ride a zebra!"
+		
+	while animal == "cheetah":
+		if my_speed >= 115:
+			return "Run!"
+		else:
+			return "Stay calm and wait!"
+			
+	while animal != "zebra" or "cheetah":
+	    return "Introduce yourself!"
+	    
+print jungle_animal("cheetah", 114.9)
